87c660aef60cbe2d537a8f3537d716a9c0dd3dfd
Anirudh-rb26 <anirudhjayakumar.business@gmail.com>
2023-05-16 17:50:19 +0300

    Firebase Initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:ordering_system/firebase_options.dart';
 import 'package:ordering_system/pages/homepage.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
